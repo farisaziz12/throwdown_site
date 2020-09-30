9f7b61585fec0e4ef1b718db9bc5d4cd6ba93d6a
farisaziz12 <farisaziz12@gmail.com>
2020-09-30 22:53:16 +0200

    chore: Full site basic setup complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d4db2bd87e5eb56b424";
+/******/ 	var hotCurrentHash = "c6b17833133ab604a8f1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
