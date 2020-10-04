7b7f51f3cf75c97d012e6ec85baf1a2406b55437
farisaziz12 <farisaziz12@gmail.com>
2020-10-04 02:05:45 +0200

    fix: quiz results fixed for if less than 3 results available

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "92cb60b88c3156110f82";
+/******/ 	var hotCurrentHash = "b2d5dfdf3cdf17c59f51";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
