215b197d407f96440412ef2a0c7f73b3586d5ffa
farisaziz12 <farisaziz12@gmail.com>
2020-11-07 21:58:46 +0100

    fix(api & team-card): fixed getTeamAthletes fucntion

--- api/index.js
+++ api/index.js
@@ -1,128 +1,146 @@
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 
 const getUser = (email) => {
-    return fetch(`https://wod-with-faris-backend.herokuapp.com/user/getuser?email=${email}`)
-                        .then(resp => resp.json())
-}
+  return fetch(
+    `https://wod-with-faris-backend.herokuapp.com/user/getuser?email=${email}`
+  ).then((resp) => resp.json());
+};
 
 const getMyTeam = (email) => {
-    return fetch(`https://wod-with-faris-backend.herokuapp.com/team/my_team?email=${email}`)
-    .then(resp => resp.json())
-}
+  return fetch(
+    `https://wod-with-faris-backend.herokuapp.com/team/my_team?email=${email}`
+  ).then((resp) => resp.json());
+};
 
-const getTeamAthletes = (id) => {
-    const data =  fetch(`https://wod-with-faris-backend.herokuapp.com/team/get_team_athletes?id=${id}`)
-            .then(resp => resp.json())
-    return data.team
-}
+const getTeamAthletes = async (id) => {
+  const response = await fetch(
+    `https://wod-with-faris-backend.herokuapp.com/team/get_team_athletes?id=${id}`
+  ).then((resp) => resp.json());
+  if (response.team) {
+    return response.team;
+  } else {
+    throw new Error(response.message);
+  }
+};
 
-const joinTeam  = async (userEmail, teamID) => {
-    const response = await fetch("https://wod-with-faris-backend.herokuapp.com/team/join_team", {
-                        method: "POST",
-                        headers: {
-                            "Content-type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            email: userEmail,
-                            team_id: teamID
-                        })
-                    }).then(resp => resp.json())
-    if  (response.email){
-        return response
-    } else {
-        throw new Error(response.message)
+const joinTeam = async (userEmail, teamID) => {
+  const response = await fetch(
+    "https://wod-with-faris-backend.herokuapp.com/team/join_team",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userEmail,
+        team_id: teamID,
+      }),
     }
-}
+  ).then((resp) => resp.json());
+  if (response.email) {
+    return response;
+  } else {
+    throw new Error(response.message);
+  }
+};
 
 const exitTeam = async (user, team, teamID) => {
-    const response  = await fetch("https://wod-with-faris-backend.herokuapp.com/team/exit_team", {
-                                method: "POST",
-                                headers: {
-                                    "Content-type": "application/json"
-                                },
-                                body: JSON.stringify({
-                                    email: user.email,
-                                    team_id: teamID
-                                })
-                            }).then(resp => resp.json())
-    if (response.message === "Delete Success")
-        return team.filter(athlete => athlete.id !== user.id)
-    else {
-        throw new Error(response.message)
+  const response = await fetch(
+    "https://wod-with-faris-backend.herokuapp.com/team/exit_team",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: user.email,
+        team_id: teamID,
+      }),
     }
-}
+  ).then((resp) => resp.json());
+  if (response.message === "Delete Success")
+    return team.filter((athlete) => athlete.id !== user.id);
+  else {
+    throw new Error(response.message);
+  }
+};
 
 const signUpUser = async (userEmail, firstName, lastName) => {
-    const response = await fetch("https://wod-with-faris-backend.herokuapp.com/user/create", {
-                                method: "POST",
-                                headers: {
-                                    Accept: "application/json",
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({
-                                    email: userEmail,
-                                    first_name: firstName,
-                                    last_name: lastName,
-                                    tokens: 0,
-                                    coach: false,
-                                }),
-                            })
-                            .then((resp) => resp.json())
-    if (response.email) {
-        return response
-    } else {
-        throw new Error(response.message)
+  const response = await fetch(
+    "https://wod-with-faris-backend.herokuapp.com/user/create",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userEmail,
+        first_name: firstName,
+        last_name: lastName,
+        tokens: 0,
+        coach: false,
+      }),
     }
-}
+  ).then((resp) => resp.json());
+  if (response.email) {
+    return response;
+  } else {
+    throw new Error(response.message);
+  }
+};
 
 const createNewTeam = async (teamName, teamCategory) => {
-    const response = await fetch(`https://wod-with-faris-backend.herokuapp.com/team/create_team`, {
-                                method: "POST",
-                                headers: {
-                                    "Content-type": "application/json"
-                                },
-                                body: JSON.stringify({
-                                    name: teamName,
-                                    category: teamCategory
-                            })
-                            })
-                            .then(resp => resp.json())
-    if (response){
-        return true
-    } else {
-        throw new Error(response.message)
+  const response = await fetch(
+    `https://wod-with-faris-backend.herokuapp.com/team/create_team`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: teamName,
+        category: teamCategory,
+      }),
     }
-}
+  ).then((resp) => resp.json());
+  if (response) {
+    return true;
+  } else {
+    throw new Error(response.message);
+  }
+};
 
 const getAllTeams = () => {
-    return fetch(`https://wod-with-faris-backend.herokuapp.com/team/all_teams`)
-            .then(resp => resp.json())
-}
+  return fetch(
+    `https://wod-with-faris-backend.herokuapp.com/team/all_teams`
+  ).then((resp) => resp.json());
+};
 
 const pushNotification = (title, message) => {
-    fetch("https://api.pushover.net/1/messages.json", {
-                    method: "POST",
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        token: process.env.NEXT_PUBLIC_PUSHOVER_PROJECT_KEY,
-                        user: process.env.NEXT_PUBLIC_PUSHOVER_USER_KEY,
-                        title: title,
-                        message: message
-                    })
-    })
-}
+  fetch("https://api.pushover.net/1/messages.json", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: process.env.NEXT_PUBLIC_PUSHOVER_PROJECT_KEY,
+      user: process.env.NEXT_PUBLIC_PUSHOVER_USER_KEY,
+      title: title,
+      message: message,
+    }),
+  });
+};
 
 module.exports = {
-    getUser,
-    getMyTeam,
-    getTeamAthletes,
-    joinTeam,
-    exitTeam,
-    signUpUser,
-    createNewTeam,
-    getAllTeams,
-    pushNotification
-}
+  getUser,
+  getMyTeam,
+  getTeamAthletes,
+  joinTeam,
+  exitTeam,
+  signUpUser,
+  createNewTeam,
+  getAllTeams,
+  pushNotification,
+};
--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,114 +1,176 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import PopPop from "react-poppop";
-import { capitalize } from "../functions"
-import { getTeamAthletes, joinTeam, exitTeam } from "../api"
-import Avatar from '@material-ui/core/Avatar';
+import { capitalize } from "../functions";
+import { getTeamAthletes, joinTeam, exitTeam } from "../api";
+import Avatar from "@material-ui/core/Avatar";
 
 export default function TeamCard(props) {
-    const { id, name, category, user } = props
-    const [team, setTeam] = useState([])
-    const [myTeam, setMyTeam] = useState(false)
-    const [showModal, setShowModal] = useState(false);
+  const { id, name, category, user } = props;
+  const [team, setTeam] = useState([]);
+  const [myTeam, setMyTeam] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-
-    useEffect(() => {
-        try {
-            (async () => {
-                const teamAthletes = getTeamAthletes(id)
-                setTeam(teamAthletes? teamAthletes : [])
-            })()
-        } catch (error) {
-            console.error(error)
-        }
-    }, [])
-
-
-    useEffect(() => {
-        if (team[0] && user) {
-            const isMyTeam = team.filter(athlete => athlete.id === user.id)
-            setMyTeam(!!isMyTeam)
-        } else {
-            setMyTeam(false)
-        }
-    }, [team, user])
-
-    const toggleShow = (show) => {
-        setShowModal(show);
-    };
-
-    const handleJoinTeam = async () => {
-        try {
-            const joinedUser = await joinTeam(user.email, id)
-            setTeam([...team, joinedUser])
-        } catch (error) {
-            console.error(error)
-        }
+  useEffect(() => {
+    try {
+      (async () => {
+        const teamAthletes = await getTeamAthletes(id);
+        setTeam(teamAthletes ? teamAthletes : []);
+      })();
+    } catch (error) {
+      console.error(error);
     }
+  }, []);
 
-    const handleExitTeam = async () => {
-        try {
-            const updatedTeam = await exitTeam(user, team, id)
-            if (updatedTeam){
-                setTeam(updatedTeam)
-            }
-        } catch (error) {
-            console.error(error)
-        }
+  useEffect(() => {
+    if (team[0] && user) {
+      const isMyTeam = team.filter((athlete) => athlete.id === user.id);
+      setMyTeam(!!isMyTeam);
+    } else {
+      setMyTeam(false);
     }
+  }, [team, user]);
 
-    return (
-        <>
-            <div className="card text-white bg-dark mb-3" style={{minWidth: "18rem"}}>
-                <div className="card-header" style={{opacity: "0.8"}}>Category: {capitalize(category)}</div>
-                <div className="card-body">
-                    <h5 className="card-title default-font">"{name}"</h5>
-                    <div>
-                        {team?
-                        <div style={{display: "inline-flex", justifyContent: "center"}}>
-                            {team.map(athlete => (
-                                <Avatar key={athlete.id} style={{marginLeft: "3%"}} alt="Athlete" src={athlete.image? athlete.image : "/images/profile_pic.jpeg" } />
-                            ))}
-                        </div>
-                        :
-                            <h5 className="text-align">No Athletes</h5>
-                        }
-                    </div>
-                    <button type="button" style={{display: "block", marginRight: "auto", marginLeft: "auto"}} onClick={() => toggleShow(true)} className="btn btn-success">Join</button>
-                </div>
-            </div>
-            <PopPop
-            position="centerCenter"
-            open={showModal}
-            closeBtn={true}
-            closeOnEsc={true}
-            onClose={() => toggleShow(false)}
-            closeOnOverlay={true}
-            >
-                <div className="more-info-modal">
-                <h1 className="text-align default-font">"{name}"</h1>
+  const toggleShow = (show) => {
+    setShowModal(show);
+  };
+
+  const handleJoinTeam = async () => {
+    try {
+      const joinedUser = await joinTeam(user.email, id);
+      setTeam([...team, joinedUser]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleExitTeam = async () => {
+    try {
+      const updatedTeam = await exitTeam(user, team, id);
+      if (updatedTeam) {
+        setTeam(updatedTeam);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="card text-white bg-dark mb-3"
+        style={{ minWidth: "18rem" }}
+      >
+        <div className="card-header" style={{ opacity: "0.8" }}>
+          Category: {capitalize(category)}
+        </div>
+        <div className="card-body">
+          <h5 className="card-title default-font">"{name}"</h5>
+          <div>
+            {team ? (
+              <div style={{ display: "inline-flex", justifyContent: "center" }}>
+                {team.map((athlete) => (
+                  <Avatar
+                    key={athlete.id}
+                    style={{ marginLeft: "3%" }}
+                    alt="Athlete"
+                    src={
+                      athlete.image ? athlete.image : "/images/profile_pic.jpeg"
+                    }
+                  />
+                ))}
+              </div>
+            ) : (
+              <h5 className="text-align">No Athletes</h5>
+            )}
+          </div>
+          <button
+            type="button"
+            style={{
+              display: "block",
+              marginRight: "auto",
+              marginLeft: "auto",
+            }}
+            onClick={() => toggleShow(true)}
+            className="btn btn-success"
+          >
+            Join
+          </button>
+        </div>
+      </div>
+      <PopPop
+        position="centerCenter"
+        open={showModal}
+        closeBtn={true}
+        closeOnEsc={true}
+        onClose={() => toggleShow(false)}
+        closeOnOverlay={true}
+      >
+        <div className="more-info-modal">
+          <h1 className="text-align default-font">"{name}"</h1>
+          <div>
+            {team ? (
+              <>
+                <h3 className="text-align default-font">Athletes:</h3>
                 <div>
-                    {team?
-                    <>
-                        <h3 className="text-align default-font">Athletes:</h3>
-                        <div>
-                            {team.map(athlete => (
-                                <div style={{display: "flex", justifyContent: "center", marginBottom: "2%"}}>
-                                    <Avatar key={athlete.id} style={{marginRight: "3%"}} alt="Athlete" src={athlete.image? athlete.image : "/images/profile_pic.jpeg" } /><p style={{marginTop: "2%"}} className="default-font">{athlete.first_name + " " + athlete.last_name}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </>
-                    :
-                        <h5 className="text-align">No Athletes</h5>
-                    }
-                    {myTeam?
-                        <button type="button" style={{display: "block", marginRight: "auto", marginLeft: "auto"}} onClick={handleExitTeam}className="btn btn-success topspace">Exit Team</button>
-                    :
-                        <button type="button" style={{display: "block", marginRight: "auto", marginLeft: "auto"}} onClick={handleJoinTeam} disabled={team.length === 4} className="btn btn-success topspace">{team.length === 4? "Team Full" : "Join Team"}</button>
-                    }
+                  {team.map((athlete) => (
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginBottom: "2%",
+                      }}
+                    >
+                      <Avatar
+                        key={athlete.id}
+                        style={{ marginRight: "3%" }}
+                        alt="Athlete"
+                        src={
+                          athlete.image
+                            ? athlete.image
+                            : "/images/profile_pic.jpeg"
+                        }
+                      />
+                      <p style={{ marginTop: "2%" }} className="default-font">
+                        {athlete.first_name + " " + athlete.last_name}
+                      </p>
+                    </div>
+                  ))}
                 </div>
-                </div>
-            </PopPop>
-        </>
-    )
+              </>
+            ) : (
+              <h5 className="text-align">No Athletes</h5>
+            )}
+            {myTeam ? (
+              <button
+                type="button"
+                style={{
+                  display: "block",
+                  marginRight: "auto",
+                  marginLeft: "auto",
+                }}
+                onClick={handleExitTeam}
+                className="btn btn-success topspace"
+              >
+                Exit Team
+              </button>
+            ) : (
+              <button
+                type="button"
+                style={{
+                  display: "block",
+                  marginRight: "auto",
+                  marginLeft: "auto",
+                }}
+                onClick={handleJoinTeam}
+                disabled={team.length === 4}
+                className="btn btn-success topspace"
+              >
+                {team.length === 4 ? "Team Full" : "Join Team"}
+              </button>
+            )}
+          </div>
+        </div>
+      </PopPop>
+    </>
+  );
 }
