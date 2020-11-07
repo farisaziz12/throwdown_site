e9f9c615eab790102c41affc7b55b8d579b99835
farisaziz12 <farisaziz12@gmail.com>
2020-11-07 23:24:38 +0100

    feat(jest-config): jest config setup

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 // Ignores node_modules and .next directories when testing
 module.exports = {
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-    transform: {
-        "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
-    },
-    moduleNameMapper: {
-        "\\.(css|less)$": "identity-obj-proxy",
-    },
-    verbose: true,
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+  verbose: true,
 };
