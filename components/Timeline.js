bceb76a3b8eaadca755f49dc67d4e65cb538c015
farisaziz12 <farisaziz12@gmail.com>
2020-11-08 14:48:18 +0100

    fix(timeline): key added to mapped timeline events

--- components/Timeline.js
+++ components/Timeline.js
@@ -3,30 +3,37 @@ import styles from "../styles/Timeline.module.css";
 import { timelineContent } from "../content/timeline";
 
 export default function Timeline() {
-  return (
-    <div>
-      <div className={styles.page}>
-        <div className={styles.timeline}>
-          <div className={styles.timelinegroup}>
-            <span className={styles.timelineyear}>
-              Saturday 5th Dec 2020
-            </span>
-            {timelineContent.map((event) => (
-              <div className={styles.timelinebox}>
-                <div className={styles.timelinedate}>
-                  <span className={styles.timelineday}>{event.time}</span>
-                  <span className={styles.timelinemonth}>Event</span>
+    return (
+        <div>
+            <div className={styles.page}>
+                <div className={styles.timeline}>
+                    <div className={styles.timelinegroup}>
+                        <span className={styles.timelineyear}>
+                            Saturday 5th Dec 2020
+                        </span>
+                        {timelineContent.map((event) => (
+                            <div
+                                key={event.time}
+                                className={styles.timelinebox}
+                            >
+                                <div className={styles.timelinedate}>
+                                    <span className={styles.timelineday}>
+                                        {event.time}
+                                    </span>
+                                    <span className={styles.timelinemonth}>
+                                        Event
+                                    </span>
+                                </div>
+                                <div className={styles.timelinepost}>
+                                    <div className={styles.timelinecontent}>
+                                        <p>{event.event}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className={styles.timelinepost}>
-                  <div className={styles.timelinecontent}>
-                    <p>{event.event}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
